adec8d769b7b1cc68c5b6655c12506311f591418
jxl-taylor <taylor@jxltech.com>
2020-02-29 17:21:18 +0800

    add task module

--- src/main/java/com/mr/rpa/assistant/data/model/SysConfig.java
+++ src/main/java/com/mr/rpa/assistant/data/model/SysConfig.java
@@ -2,11 +2,15 @@ package com.mr.rpa.assistant.data.model;
 
 import lombok.Data;
 
+import java.io.File;
+
 /**
  * Created by feng on 2020/2/5 0005
  */
 @Data
 public class SysConfig {
+
+	private static String DEFAULT_BOT_FILE_DIR = System.getProperty("user.dir");
 
 	private String adminUsername;
 	private String adminPassword;
@@ -15,10 +19,10 @@ public class SysConfig {
 	private String mailEmailAddress;
 	private String mailEmailPassword;
 	private Boolean mailSslCheckbox = false;
-	private String taskFilePath = "D:/MR-ROBOT/kjb";
-	private String logPath = "D:/MR-ROBOT/log";
+	private String taskFilePath = DEFAULT_BOT_FILE_DIR + File.separator + "kjb";
+	private String logPath = DEFAULT_BOT_FILE_DIR + File.separator + "log";
 	private String controlServer;
-	private String dbPath = "D:/MR-ROBOT/database";
+	private String dbPath = DEFAULT_BOT_FILE_DIR + File.separator + "database";
 	private Integer miniteErrorLimit = 10;
 	private Integer runningLimit = 100;
 
