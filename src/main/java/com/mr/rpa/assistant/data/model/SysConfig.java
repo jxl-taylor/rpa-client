da74cac6f8d516e4186c787bebd42fa474524973
jiang.feng <jiang.feng@huifu.com>
2020-04-24 10:43:21 +0800

    update

--- src/main/java/com/mr/rpa/assistant/data/model/SysConfig.java
+++ src/main/java/com/mr/rpa/assistant/data/model/SysConfig.java
@@ -18,6 +18,7 @@ public class SysConfig {
 
 	//	private static String DEFAULT_BOT_FILE_DIR = System.getProperty("user.dir");
 	private static String BOT_FILE_DIR_NAME = "recfile";
+	private static String DEFAULT_BOT_CONTROL_SERVER = "https://api.rpalinker.com";
 
 	private static String BOT_FILE_DIR = System.getProperty("user.dir") + File.separator + BOT_FILE_DIR_NAME;
 
@@ -34,7 +35,7 @@ public class SysConfig {
 	private String taskFilePath = BOT_FILE_DIR + File.separator + "mbot";
 	private String taskFilePathTmp = taskFilePath + File.separator + "tmp";
 	private String logPath = BOT_FILE_DIR + File.separator + "log";
-	private String controlServer = "";
+	private String controlServer = DEFAULT_BOT_CONTROL_SERVER;
 	//注册信息，通过控制中心查询获得，无连接或者没有注册则无此信息
 	private String userType = "";
 	private String companyName = "";
